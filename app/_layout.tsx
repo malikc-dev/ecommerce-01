ebbab40e06808f3dafd6ad7c7ace91b0612bb048
malikc-dev <malikcisse.dev@gmail.com>
2024-08-16 00:03:15 +0000

    reorganize

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,8 @@ import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
 import "react-native-reanimated";
+import { useEffect } from "react";
 
 export {
   // Catch any errors thrown by the Layout component.
